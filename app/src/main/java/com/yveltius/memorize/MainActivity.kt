052d1bb4fbba29d86e53a0f1814f05e7a2bc10ca
Zac Murdaugh <zacmurdaugh@gmail.com>
2025-11-25 20:56:46 -0600

    Removed unused imports in MainActivity

--- app/src/main/java/com/yveltius/memorize/MainActivity.kt
+++ app/src/main/java/com/yveltius/memorize/MainActivity.kt
@@ -3,15 +3,10 @@ package com.yveltius.memorize
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import com.yveltius.memorize.ui.theme.AppTheme
 import com.yveltius.memorize.features.addverse.screens.AddVerseScreen
 import com.yveltius.memorize.ui.screens.ChooseNextWordScreen
 import com.yveltius.memorize.ui.screens.VerseListScreen
