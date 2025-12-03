572ac311ea99e042c946ae85c09d4fcf2222f40f
Zac Murdaugh <zacmurdaugh@gmail.com>
2025-12-02 20:45:25 -0600

    Initial Internal Build!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yveltius.memorize"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
