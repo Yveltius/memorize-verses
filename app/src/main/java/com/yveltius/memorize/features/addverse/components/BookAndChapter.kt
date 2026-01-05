bb87a1a00feae2816eaedb4538617a2c26aa1408
Zac Murdaugh <zacmurdaugh@gmail.com>
2026-01-05 15:09:36 -0600

    Set default capitalization for Book in AddVerse

--- app/src/main/java/com/yveltius/memorize/features/addverse/components/BookAndChapter.kt
+++ app/src/main/java/com/yveltius/memorize/features/addverse/components/BookAndChapter.kt
@@ -3,6 +3,7 @@ package com.yveltius.memorize.features.addverse.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -39,7 +41,8 @@ fun BookAndChapter(
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next
+                imeAction = ImeAction.Next,
+                capitalization = KeyboardCapitalization.Sentences
             ),
             label = {
                 Text(text = stringResource(R.string.book))
